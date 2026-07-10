1bbb1223e0e1dfc60ed25546a6529d0dd1733f18
Ewazer <ewazer.com@gmail.com>
2026-07-10 18:57:30 +0200

    Bump version from 3.1.2 to 3.1.3

--- chesscore/chess_game.py
+++ chesscore/chess_game.py
@@ -5,7 +5,7 @@ except ImportError:
     from constants import *
     from constants import __all__ as _constants_all
 
-__version__ = "3.1.2"
+__version__ = "3.1.3"
 __author__ = "Leroux Lubin"
 
 __all__ = ["ChessCore", "Board", "MoveGen", "GameState", "ChessDisplay", *_constants_all]
